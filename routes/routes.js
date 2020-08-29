6c49f035439775d3615bd13554001138c1fc8603
Nick <nicholasroman86@gmail.com>
2020-08-29 14:09:53 -0400

    Made routes folder and created routes.js inside of the folder. Also added a post request to send account creation data to database.

---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+var con = require('.MySQLConnection');
+
+module.exports = function (app) {
+    app.post("/views/createacc", function (req, res) {
+        console.log("Creating account");
+        console.log(req.body);
+
+        var dbQuery = "INSERT INTO funnymoney (UserName, Password, Email, Phone, FullName) VALUES (?,?,?,?,?)";
+
+        connection.query(dbQuery, [req.body.user_name, req.body.account_password, req.body.account_email, req.body.account_phone, req.body.firstandlast_name], function (err, result) {
+            if (err) throw err;
+            console.log("Account successfully saved!");
+            res.end();
+        });
+    });
+}
