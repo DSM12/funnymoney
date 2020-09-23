c92c57c80a90340f2af8ec47d6b274134dcda8ec
Danny Montano <dsmontano12@gmail.com>
2020-09-23 00:06:34 -0400

    build completed

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ router.use("/api/transactions", transactionRoutes);
 router.use("/api", userRoutes);
 
 // If no API routes are hit, send the React app[]
-router.use((req, res) =>
-  res.sendFile(path.join(__dirname, "../client/public/index.html"))
-);
+router.use("*", (req, res) => {
+  console.log("anythigldlak");
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+});
 module.exports = router;
